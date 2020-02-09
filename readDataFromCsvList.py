bf45aa537c779077ad6998c0fa5a1b1b3e17cd00
Ryan Engle <rdengle@gmail.com>
2020-02-09 14:38:40 -0500

    Implements reading a list of CSVs into a dataframe

---
+++ readDataFromCsvList.py
@@ -0,0 +1,59 @@
+#
+# This file is part of the Air Force Institute of Technology (AFIT) 
+# Resilient Sensor Grig (RSG) Extract, Transform, Load (ETL) Tool.
+#
+# Written by:
+#               Ryan D. L. Engle
+#               ryan.engle@afit.edu, rdengle@gmail.com
+#               Air Force Institute of Technology
+#               Department of Systems Engineering & Management (ENV)
+#               2950 Hobson Way
+#               Wright Patterson AFB, Ohio  45433-7765  USA
+#
+# This source code is property of the United States Government.
+#
+# NO PART OF THIS PROGRAM MAY BE COPIED, REPRODUCED, OR DUPLICATED WITHOUT
+# THE EXPRESSED WRITTEN PERMISSION FROM AFIT/ENV.
+#
+# # This software is provided "AS IS" and the author disclaims all warranties with 
+# regard to this software. In no event shall the author be liable for any indirect 
+# or consequential damages arising out of, or in connection with, the use of this 
+# software. USE AT YOUR OWN RISK.
+#
+# Last Modified: 2020 0209 1430
+
+import pandas as pd
+import sys
+import os.path as ospath
+
+import readDataFromACsv
+
+def readDataFromCsvList(csvList = None):
+    '''
+            Reads data from multiple CSV files into pandas dataframe
+    '''
+    runningList = []
+    for aCsv in csvList:
+        runningList.append(readDataFromACsv.readDataFromACsv(aCsv))
+    
+    df = None
+    try:
+        df = pd.concat(runningList)
+    except Exception as e:
+        errMsg = 'ERROR readDataFromCsvList(): Line {}: '.format(sys.exc_info()[-1].tb_lineno) 
+        errMsg += e.message 
+        print errMsg
+        df = None
+
+    return df
+
+if __name__ == "__main__":
+    # Path to data for Resilient Smart Grid DDDAS (UMiami)
+    pathToData = 'C:\\Users\\rdeng\\Documents\\dev\\rsg-database\\data\\Homestead\\2007'
+    aCsv = 'FAWN_report.csv'
+
+    aCsvFile = ospath.join(pathToData, aCsv)
+    df = readDataFromACsv(aCsvFile)
+
+    if df is not None:
+        print df.keys()
