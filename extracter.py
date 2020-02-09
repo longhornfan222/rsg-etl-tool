c9136e85e56e19dec680f6322d347e6efa15af10
Ryan Engle <rdengle@gmail.com>
2020-02-09 14:39:57 -0500

    Implements the extract portion of ETL

---
+++ extracter.py
@@ -0,0 +1,42 @@
+#
+# This file is part of the Air Force Institute of Technology (AFIT) 
+# Resilient Sensor Grig (RSG) Extract, Transform, Load (ETL) Tool.
+#
+# Written by:
+#               Ryan D. L. Engle
+#               ryan.engle@afit.edu, rdengle@gmail.com
+#               Air Force Institute of Technology
+#               Department of Systems Engineering & Management (ENV)
+#               2950 Hobson Way
+#               Wright Patterson AFB, Ohio  45433-7765  USA
+#
+# This source code is property of the United States Government.
+#
+# NO PART OF THIS PROGRAM MAY BE COPIED, REPRODUCED, OR DUPLICATED WITHOUT
+# THE EXPRESSED WRITTEN PERMISSION FROM AFIT/ENV.
+#
+# # This software is provided "AS IS" and the author disclaims all warranties with 
+# regard to this software. In no event shall the author be liable for any indirect 
+# or consequential damages arising out of, or in connection with, the use of this 
+# software. USE AT YOUR OWN RISK.
+#
+# Last Modified: 2020 0209 1400
+import findData
+import readDataFromACsv
+import readDataFromCsvList
+
+def doExtract(dataFolderLocation=None, searchAllSubFolders=True):
+    '''
+        Manages the Extraction portion of ETL
+    '''
+    csvList = findData.findData(dataFolderLocation, searchAllSubFolders)
+    df = readDataFromCsvList.readDataFromCsvList(csvList)
+
+    return df
+
+if __name__ == "__main__":
+    pathToData = 'C:\\Users\\rdeng\\Documents\\dev\\rsg-database\\data\\Homestead\\2007'
+    
+    result = doExtract(pathToData, False)
+
+    print result.info()
