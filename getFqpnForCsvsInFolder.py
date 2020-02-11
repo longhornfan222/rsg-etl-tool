0e9baaa9e586e8ab2b67b256e769530412b06f75
Ryan Engle <rdengle@gmail.com>
2020-02-10 20:33:59 -0500

    Refactors out into its own file

---
+++ getFqpnForCsvsInFolder.py
@@ -0,0 +1,57 @@
+#
+# This file is part of the Air Force Institute of Technology (AFIT) 
+# Resilient Sensor Grid (RSG) Extract, Transform, Load (ETL) Tool.
+#
+# Written by:
+#               Ryan D. L. Engle
+#               ryan.engle@afit.edu, rdengle@gmail.com
+#               Air Force Institute of Technology
+#               Department of Systems Engineering & Management (ENV)
+#               2950 Hobson Way
+#               Wright Patterson AFB, Ohio  45433-7765  USA
+#
+# This source code is property of the United States Government.
+#
+# NO PART OF THIS PROGRAM MAY BE COPIED, REPRODUCED, OR DUPLICATED WITHOUT
+# THE EXPRESSED WRITTEN PERMISSION FROM AFIT/ENV.
+#
+# # This software is provided "AS IS" and the author disclaims all warranties with 
+# regard to this software. In no event shall the author be liable for any indirect 
+# or consequential damages arising out of, or in connection with, the use of this 
+# software. USE AT YOUR OWN RISK.
+#
+# Last Modified: 2020 0210 0805
+
+import os, sys
+
+def getFqpnForCsvsInFolder(folder=None):
+    '''
+        Searches a folder and returns a list of fully qualified paths to the CSVs
+    '''
+    if folder is None:
+        print 'WARNING: getFqpnForCsvsInFolder(): Folder is undefined. Returning'
+        return
+
+    if not os.path.isdir(folder):
+        print 'WARNING: getFqpnForCsvsInFolder(): Folder is not a valid folder:\n\t' \
+            + folder 
+        return
+
+    allFqpnCsvs = None
+    try:
+        # Get all files in specified path
+        allFilenames = os.listdir(folder)
+        # Get the applicable CSVs and build the fully qualified path name
+        allFqpnCsvs = [ os.path.join(folder, filename) for filename in allFilenames if filename.endswith('.csv') ]
+    except Exception as e:
+        errMsg = 'ERROR getFqpnForCsvsInFolder(): Line {}: '.format(sys.exc_info()[-1].tb_lineno) 
+        errMsg = '\tSearch folder was: ' + str(folder)
+        errMsg += e.message 
+        print errMsg
+        allFqpnCsvs = None    
+    
+    return allFqpnCsvs
+
+if __name__ == "__main__":
+    pathToData = 'C:\\Users\\rdeng\\Documents\\dev\\rsg-database\\data\\Homestead\\2007'    
+    print getFqpnForCsvsInFolder(pathToData)
