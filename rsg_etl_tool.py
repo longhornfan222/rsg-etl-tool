72a72e385e5b40d79c3a5ac35fa0b16197ca5d93
Ryan Engle <rdengle@gmail.com>
2020-02-10 22:12:25 -0500

    Implements Open Folder button
    - Does not fully implement state checking (other buttons are disabled)

--- rsg_etl_tool.py
+++ rsg_etl_tool.py
@@ -20,16 +20,18 @@
 # or consequential damages arising out of, or in connection with, the use of this 
 # software. USE AT YOUR OWN RISK.
 #
-__version__ = '2020 0210 1750 Eastern'
+__version__ = '2020 0210 2211 Eastern'
 ###############################################################################
 from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtCore import pyqtSignal
-import sys
+import os, sys
 
 import rsg_etl_tool_ui
 
+import findData
+
 class RsgEtlApp(QtWidgets.QMainWindow, rsg_etl_tool_ui.Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, pathToData=None):
         # Enable access to variables and methods in rsg_etl_tool_ui.py
         super(self.__class__, self).__init__()
 
@@ -40,20 +42,106 @@ class RsgEtlApp(QtWidgets.QMainWindow, rsg_etl_tool_ui.Ui_MainWindow):
         # Signals are connected to slots using connect(slotName)
         # File menu - Slots
         # Example
-        # self.actionLoad_Signal_Set.triggered.connect(self.loadSignalSet)
+        # self.open_folder.triggered.connect(self.slotOpenFolder)
         
-        # Frame 1 - Buttons (slots)
-        #self.butLoad.clicked.connect(self.loadSmsFiles)
+        # Frame Top Frame - Buttons (slots)
+        self.butOpenFolder.clicked.connect(self.slotButOpenFolderClicked)
+
+        # Set initial GUI state
+        # Disable buttons
+        self.butNewTable.setEnabled(False)
+        self.butPopulate.setEnabled(False)
+        # Reset progress bar
+        self.pbProgressBar.setMaximum(1)
+        self.pbProgressBar.setValue(0)
+
+        # Other variables
+        self.dataFolder = pathToData # Not fully implemented
+        self.fqpnFileList = []
+###############################################################################
+
+    def checkState(self):
+        '''
+            Checks various GUI states and updates functionality accordingly
+            TODO
+        '''
+        self.checkStatePopulate()
+
+    def checkStatePopulate(self):
+        '''
+            Checks whether Populate button should be enabled
+        '''
+        msg = 'Check 1. A dataFolder is selected, \n2. one or more files are selected, \n3. and a table is selected'
+        print msg
+
+    def getFolder(self, previousFolder=None):
+        '''
+            Prompts user with dialog to select folder
+        '''
+        widgetTitle = 'Data Folder'
+        if previousFolder is None:
+            previousFolder = '.'
+    
+        fqpn = None
+        # Option 1
+        fqpn, cwd = QtWidgets.QFileDialog.getOpenFileName(self, widgetTitle, previousFolder)
+        # Option 2
+        #folder = QtWidgets.QFileDialog.getExistingDirectory(self, widgetTitle, previousFolder)
+        # Get the folder name 
+        # WARNING Remove if using option 2 above
+        folder = os.path.dirname(fqpn)
+
+        # determine if folder is valid
+        is_a_directory = os.path.isdir(folder)
+        directory_exists = os.path.exists(folder)
+        if (is_a_directory is False) or (directory_exists is False):            
+            msg = 'getFolder(): WARNING: Invalid directory (' + str(folder) +') selected.'
+            print msg
+            folder = None
+        
+        return folder    
+
+    def slotButOpenFolderClicked(self):
+        '''
+            Slot to handle Open Folder button click
+        '''
+        folder = self.getFolder(self.dataFolder)
+        if folder is None:
+            # Verify this is the expected behavior.. Consider cancel action
+            return
+
+        # Update GUI
+        self.checkState() # TODO
+        self.dataFolder = folder
+        self.lblSelectedDataFolder.setText(self.dataFolder)
+        # Fill list with files & update gui list
+        # Needs more error checking
+        self.fqpnFileList = findData.findData(self.dataFolder)
+        self.updateLstFilesInFolder()
+
+
+    def updateLstFilesInFolder(self):
+        '''
+            Updates lstFilesInFolder using fqpn data from self.fileList
+        '''
+        self.lstFilesInFolder.clear()
+        for f in self.fqpnFileList:
+            self.lstFilesInFolder.addItem(os.path.basename(f))
+
+        
 
 
 
 
 def start(pathToData=None):
+    '''
+        Starts the application execution loop
+        TODO fully implement pathToData
+    '''
     app = QtWidgets.QApplication(sys.argv)
-    form = RsgEtlApp()
+    form = RsgEtlApp(pathToData)
     form.show()
     app.exec_()
-
 
 if __name__ == "__main__":
     start()
