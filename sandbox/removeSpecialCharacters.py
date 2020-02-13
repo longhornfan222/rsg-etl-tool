490ae5bfe38fdc1895e1efed0bdff51260f5f73e
Ryan Engle <rdengle@gmail.com>
2020-02-13 11:26:17 -0500

    Adds to sandbox

---
+++ sandbox/removeSpecialCharacters.py
@@ -0,0 +1,10 @@
+specialCharactersList = ['~', '`', '!', '@']
+
+def removeSpecialChars(dangerousString):
+    '''
+    Removes Special Characters from dangerousString
+
+    Returns saferString
+    '''
+    singleUnderscore = '_'
+    doubleUnderscore = '__'
