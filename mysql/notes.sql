bf13db0c3f51707d90722baabc59fc1bb01e5f77
Ryan Engle <rdengle@gmail.com>
2020-02-17 21:29:10 -0500

    Updates with fawn examples & syntax fixes

--- mysql/notes.sql
+++ mysql/notes.sql
@@ -3,11 +3,13 @@ create schema UMiami;
 use umiami;
 
 select * from generator;
-
 describe generator;
+select count(datetimeSampled) from generator;
 
+select * from fawn;
+describe fawn;
+select count(datetimeSampled) from fawn;
 
-select count(`datetimeSampled`) from generator;
 
 select * from users;
 
@@ -16,7 +18,7 @@ select * from userspk;
 select * from userscompositepk;
 
 INSERT INTO Generator (FAWN_Station, Observation_Time, 60cm_T_air___F__, dateTimeSampled) 
-VALUES ('Homestead', 'Mar 1 2007  12:15 AM', '70.39', '2007-03-01 00:15:00')
+VALUES ('Homestead', 'Mar 1 2007  12:15 AM', '70.39', '2007-03-01 00:15:00');
 
 INSERT INTO Generator (FAWN_Station, Observation_Time, 60cm_T_air___F__, 2m_T_air___F__, 
     10m_T_air___F__, __10cm_T_soil___F__, 2m_DewPt___F__, 2m_Rel_Hum___pct__, 2m_Rain___in__, 
@@ -25,4 +27,4 @@ INSERT INTO Generator (FAWN_Station, Observation_Time, 60cm_T_air___F__, 2m_T_ai
 VALUES ('Homestead', 'Mar 1 2007  12:15 AM', '70.39', '70.79', 
 '70.5', '68.68', '68.12', '91.0', '0.0', 
 '1.16', NULL, NULL, NULL, 
-'102.9', NULL, '68.96', '2007-03-01 00:15:00')
+'102.9', NULL, '68.96', '2007-03-01 00:15:00');
